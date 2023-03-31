f34cd3d6d202ceac7bc34042704186f56c9807a7
techdave <klatzdavie19@gmail.com>
2023-03-31 20:58:12 +0300

    add error middleware

---
+++ middlewares/errors.js
@@ -0,0 +1,18 @@
+const ErrorHandler = require('../utils/errorHandler');
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+
+  let error = { ...err };
+
+  error.message = err.message;
+
+  res
+    .status(err.statusCode)
+    .json({
+      success: false,
+      error,
+      message: error.message,
+      stack: error.stack,
+    });
+};

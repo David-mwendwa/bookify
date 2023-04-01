0dd8fde0a1da8b2eb579a388139d34094b69f137
techdave <klatzdavie19@gmail.com>
2023-04-01 15:43:25 +0300

    dispatch rooms action in getServerSideProps()

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Home from '../components/Home';
 import Layout from '../components/layout/Layout';
+import { getRooms } from '../redux/actions/roomActions';
+import { wrapper } from '../redux/store';
 
 export default function Index() {
   return (
@@ -8,3 +10,18 @@ export default function Index() {
     </Layout>
   );
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ req, res }) => {
+      await store.dispatch(getRooms(req));
+    }
+);
+
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   async ({ req, store }) => {
+//     await store.dispatch(getRooms(req));
+//   }
+// );
+
+

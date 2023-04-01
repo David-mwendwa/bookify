16bd8ac02dbe55f4e981fd92a2f57a09fa7e4f91
techdave <klatzdavie19@gmail.com>
2023-04-01 13:31:17 +0300

    add app homepage

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div className='container'>
         <div className='col-3 p-0'>
           <div className='navbar-brand'>
-            <Image
+            <img
               style={{ cursor: 'pointer' }}
               src='./images/bookit_logo.png'
               alt='BookIT'

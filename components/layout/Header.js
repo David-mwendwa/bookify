33156cf7f555ca25085cd53763fff649eb75bad2
techdave <klatzdavie19@gmail.com>
2023-04-04 09:24:01 +0300

    show user in the header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -7,8 +7,7 @@ import React, { useEffect } from 'react';
 
 const Header = () => {
   const dispatch = useDispatch();
-  const authstate = useSelector((state) => state.auth);
-  console.log('AUTHSTATE', authstate);
+  const { loading, user, error } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(loadUser());
@@ -29,11 +28,49 @@ const Header = () => {
         </div>
 
         <div className='col-3 mt-3 mt-md-0 text-center'>
-          <Link
-            href='/login'
-            className='btn btn-danger px-4 text-white login-header-btn float-right'>
-            Login
-          </Link>
+          {user ? (
+            <div className='ml-4 dropdown d-line'>
+              <a
+                className='btn dropdown-toggle mr-4'
+                id='dropDownMenuButton'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'>
+                <figure className='avatar avatar-nav'>
+                  <img
+                    src={user?.avatar?.url}
+                    alt={user?.name}
+                    className='rounded-circle'
+                  />
+                </figure>
+                <span>{user?.name.replace(/\s\w+/, '')}</span>
+              </a>
+              <div
+                className='dropdown-menu'
+                aria-labelledby='dropDownMenuButton'>
+                <Link href='/bookings/me' className='dropdown-item'>
+                  Bookings
+                </Link>
+                <Link href='/me' className='dropdown-item'>
+                  Profile
+                </Link>
+                <Link href='/me/update' className='dropdown-item'>
+                  Update
+                </Link>
+                <Link href='/' className='dropdown-item'>
+                  Logout
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link
+                href='/login'
+                className='btn btn-danger px-4 text-white login-header-btn float-right'>
+                Login
+              </Link>
+            )
+          )}
         </div>
       </div>
     </nav>

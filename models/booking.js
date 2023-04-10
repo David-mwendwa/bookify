67aa7fc70e737bc2e9a6606634f3210ecc587c00
techdave <klatzdavie19@gmail.com>
2023-04-10 12:12:14 +0300

    create booking model

---
+++ models/booking.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const bookingSchema = new mongoose.Schema({
+  room: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Room',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  checkInDate: {
+    type: Date,
+    required: true,
+  },
+  checkOutDate: {
+    type: Date,
+    required: true,
+  },
+  amountPaid: {
+    type: Number,
+    required: true,
+  },
+  daysOfStay: {
+    type: Number,
+    required: true,
+  },
+  paymentInfo: {
+    id: { type: String },
+    status: { type: String },
+  },
+  paidAt: {
+    type: Date,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Booking ||
+  mongoose.model('Booking', bookingSchema);

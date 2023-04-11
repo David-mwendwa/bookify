c72d54ba8fe4a8140d654fd5e32db14a7beed29e
techdave <klatzdavie19@gmail.com>
2023-04-11 07:23:29 +0300

    handle room availability endpoint

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -14,7 +14,7 @@ export const newBooking = catchAsyncErrors(async (req, res, next) => {
     daysOfStay,
     amountPaid,
     paymentInfo,
-  } = req.body;
+  } = req.query;
 
   const booking = await Booking.create({
     room,
@@ -29,3 +29,39 @@ export const newBooking = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({ success: true, data: booking });
 });
+
+// check room booking availability => /api/bookings/check
+export const checkRoomBookingAvailability = catchAsyncErrors(
+  async (req, res, next) => {
+    let { roomId, checkInDate, checkOutDate } = req.query;
+
+    checkInDate = new Date(checkInDate);
+    checkOutDate = new Date(checkOutDate);
+
+    const bookings = await Booking.find({
+      room: roomId,
+      $and: [
+        {
+          checkInDate: {
+            $lte: checkOutDate,
+          },
+        },
+        {
+          checkOutDate: {
+            $gte: checkInDate,
+          },
+        },
+      ],
+    });
+
+    // check if there is any booking available
+    let isAvailable;
+    if (bookings && bookings.length === 0) {
+      isAvailable = true;
+    } else {
+      isAvailable = false;
+    }
+
+    res.status(200).json({ success: true, data: bookings });
+  }
+);

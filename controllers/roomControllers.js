c460accbf2e4450fa0db13c8f4339b259ea5d501
techdave <klatzdavie19@gmail.com>
2023-04-13 07:42:27 +0300

    create new review endpoint

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -62,3 +62,38 @@ export const deleteRoom = catchAsyncErrors(async (req, res, next) => {
   }
   res.status(200).json({ success: true, message: 'Room is deleted' });
 });
+
+// create a new review => /api/review
+export const createRoomReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, roomId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const room = await Room.findById(roomId);
+  const isReviewed = room.reviews.find(
+    (r) => r.user.toString() === req.user._id.toString()
+  );
+  if (isReviewed) {
+    room.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    room.reviews.push(review);
+    room.numOfReviews = room.reviews.length;
+  }
+  room.rating =
+    room.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    room.reviews.length;
+
+  await room.save({ validateBeforeSave: false });
+
+  res.status(200).json({ success: true });
+});

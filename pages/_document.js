f3454223fc8adebdcae6e02854a9287b82d26551
techdave <klatzdavie19@gmail.com>
2023-04-29 20:00:02 +0300

    update document

--- pages/_document.js
+++ pages/_document.js
@@ -14,33 +14,10 @@ export default function Document() {
           type='text/css'
           href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
         />
-        {/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link> */}
       </Head>
       <body>
         <Main />
         <NextScript />
-
-        {/* BOOTSTRAP v4.1 JS  !NOT RESPONDING!!! */}
-        {/* <script
-          src='https://code.jquery.com/jquery-3.3.1.slim.min.js'
-          integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo'
-          crossorigin='anonymous'
-          async></script>
-        <script
-          src='https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js'
-          integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49'
-          crossorigin='anonymous'
-          async></script>
-        <script
-          src='https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js'
-          integrity='sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy'
-          crossorigin='anonymous'
-          async></script>
-
-        <script
-          src='https://kit.fontawesome.com/a076d05399.js'
-          crossorigin='anonymous'
-          async></script> */}
         <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js'></script>
         <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>
         <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>

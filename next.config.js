147914a913e4abb51928456593a4f5dc61a9f78b
techdave <klatzdavie19@gmail.com>
2023-05-01 10:25:55 +0300

    prepare the app for deployment

--- next.config.js
+++ next.config.js
@@ -11,15 +11,16 @@ const nextConfig = {
       'pk_test_51Imu8iKOyrEmScQWPjLK1SnUf9PbG8rllu8jmde1hV3ACQ6Qgmah6LUbIY6ihb89T4drjJvpcopn6V5HMwKvZEef00WiY46IBc',
     STRIPE_SECRET_KEY:
       'sk_test_51Imu8iKOyrEmScQW3cepN6ppj7EXKrrhf3VTtEkBihn9Kt2o8S5PH4Or5w7VARuWOmF6HTsbU8LrbiT2g6oGFnid00mvREAaRm',
-    STRIPE_WEBHOOK_SECRET:
-      'whsec_ab30dea7e2d1614b3a156c3f35b6529a44cb28b9f139638074dba87039a245cb',
+    // STRIPE_WEBHOOK_SECRET:
+    //   'whsec_ab30dea7e2d1614b3a156c3f35b6529a44cb28b9f139638074dba87039a245cb',
+    STRIPE_WEBHOOK_SECRET: 'whsec_Q6VUyFK5YrhkFIUCYklgyO0Hkfg0uWbl',
 
     CLOUDINARY_CLOUD_NAME: 'techdave',
     CLOUDINARY_API_KEY: '739477856127672',
     CLOUDINARY_API_SECRET: 'tCQKDpHPjSQ3buzVMUbm_O_Bab4',
 
     NEXTAUTH_SECRET: 'Xn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y',
-    NEXTAUTH_URL: 'http://localhost:3000/',
+    NEXTAUTH_URL: 'https://bookify.vercel.app',
 
     SMTP_HOST: 'sandbox.smtp.mailtrap.io',
     SMTP_PORT: 2525,

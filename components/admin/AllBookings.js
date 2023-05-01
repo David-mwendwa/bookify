89a47076f64b83bb0ff49fbabc875d9d4d68f3bd
techdave <klatzdavie19@gmail.com>
2023-05-01 12:56:31 +0300

    shrink oversized files

--- components/admin/AllBookings.js
+++ components/admin/AllBookings.js
@@ -4,7 +4,7 @@ const AllBookings = () => {
   return (
     <div>
       <div className='alert alert-danger'>
-        All booking component emptied because of vercel deployment error
+        Admin bookings component emptied because of vercel deployment error
         <br />
         <small>
           {`The Serverless Function "admin/bookings" is 51.9mb which exceeds the
